16025edf52b3795ebceecdb3514e2d1663cc62e9
Amardeep <vermaamardeep86@gmail.com>
2024-01-13 21:38:55 +0530

    Day-1 C# Challenge

---
+++ Hello world/Hello world/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int var = 10;
+            Console.WriteLine(var);
+            Console.ReadLine();
+        }
+    }
+}
