96aa63db1aec2107d4de16f2b3c6ab9760488b4f
Amardeep <vermaamardeep86@gmail.com>
2024-02-03 14:36:16 +0530

    Day 17 C# Challenge

---
+++ Day 18/Day 18/Program.cs
@@ -0,0 +1,175 @@
+﻿// factorial
+/*
+
+{
+    Console.Write("Enter a number :");
+    int num = int.Parse(Console.ReadLine());
+    int fact = 1;
+    for (int i = 1; i <=num; i++) {
+        fact = fact * i;   
+    }
+    Console.WriteLine(fact);
+
+
+}
+
+// pattern printing
+
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
+
+*
+**
+***
+****
+*****
+
+
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
+
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = i; j <= 5; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+{
+    for (int i = 1; i <=5; i++)
+    {
+        for (int j = i; j <=5; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+// basic calculator
+
+{
+    double res;
+    Console.Write("Enter first number:");
+    double num1 = double.Parse(Console.ReadLine());
+    Console.Write("Enter second number:");
+    double num2 = double.Parse(Console.ReadLine());
+    Console.Write("Enter operator");
+    string op = Console.ReadLine();
+
+    switch (op)
+    {
+        case "+":
+            res = num1 + num2;
+            Console.WriteLine("Addition:"+res);
+            break;
+            case "-":
+            res = num1 - num2;
+            Console.WriteLine("Subtraction:" + res);
+            break;
+            case "*":
+            res = num1 * num2;
+            Console.WriteLine("Multiplication:" + res);
+            break;
+            case "/":
+            res = num1 / num2;
+            Console.WriteLine("Division:" + res);
+            break;
+
+    }
+
+}{
+    Console.Write("Enter a number :");
+    int num = int.Parse(Console.ReadLine());
+    int fact = 1;
+    for (int i = 1; i <=num; i++) {
+        fact = fact * i;   
+    }
+    Console.WriteLine(fact);
+
+
+}
+
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+}
+
+using System.Transactions;
+
+{ int res;
+    Console.Write("Enter first  number:");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.Write("Enter second no:");
+    int num2 = int.Parse(Console.ReadLine());
+    string op = Console.ReadLine();
+    switch (op)
+    {
+        case '*':
+                Console.WriteLine("Multiplication = " + res);
+            break;
+    
+
+    }
+}
+
+
+
+
+{
+    double res;
+    Console.Write("Enter first number:");
+    double num1 = double.Parse(Console.ReadLine());
+    Console.Write("Enter second number:");
+    double num2 = double.Parse(Console.ReadLine());
+    Console.Write("Enter operator: ");
+    char op = char.Parse(Console.ReadLine());
+
+    switch (op)
+    {
+        case '+':
+            res = num1 + num2;
+            Console.WriteLine("Addition:" + res);
+            break;
+        case '-':
+            res = num1 - num2;
+            Console.WriteLine("Subtraction:" + res);
+            break;
+
+
+    }
+
+}
+*/
+
+
