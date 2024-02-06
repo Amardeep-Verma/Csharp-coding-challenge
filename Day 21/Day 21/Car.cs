ec0bad4bd4a5a9a34a1ebde04ee708dc4462d2c3
Amardeep <vermaamardeep86@gmail.com>
2024-02-06 22:48:21 +0530

    Day 21 C# Challenge

---
+++ Day 21/Day 21/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_21
+{
+    internal class Car
+    {
+        // constructor
+        public Car() {
+            Console.WriteLine("Car was created");
+
+
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Car is stopped");
+        }
+    }
+}
---
+++ Day 21/Day 21/Program.cs
@@ -0,0 +1,45 @@
+﻿/*namespace Day_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <=5; i++)
+            {
+                for (int j = 1; j <=i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+*/
+
+
+using Day_21;
+
+{
+    // a class in C# is a blueprint for creating objects.
+    // it defines a set of properties (data attributes)
+    // and methods (functions) that an object of that class will have
+
+    Car audi = new Car();
+    audi.Drive();
+
+    Car bmw = new Car();
+    bmw.Drive();
+
+    Console.WriteLine("Press 1 to stop the car");
+    string userInput = Console.ReadLine();
+    if(userInput == "1")
+    {
+        audi.Stop();
+    }
+    else
+    {
+        Console.WriteLine("Car drives indefinetely");
+    }
+}
