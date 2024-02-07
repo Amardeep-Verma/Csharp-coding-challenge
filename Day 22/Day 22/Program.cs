f788fcc50f447db146678734e28e37c0618d9695
Amardeep <vermaamardeep86@gmail.com>
2024-02-07 21:36:03 +0530

    Day 22 C# Challenge

---
+++ Day 22/Day 22/Program.cs
@@ -0,0 +1,65 @@
+﻿// Loops 
+
+/*
+
+// for loop to compute sum of first n natural numbers
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+{
+    Console.WriteLine("Enter number:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int sum=0;
+    for (int i = 1;i <= number; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine($"Sum of {number} is {sum}");
+}
+
+
+
+// for loop with multiple initialization and iterator expression
+
+{
+    for (int i = 0,j = 0; i+j <= 5; i++ , j++){
+        Console.WriteLine($"i={i},j={j}");
+
+    }
+}
+
+
+
+// for loop without initialization and iterator statement
+{
+    int i = 1;
+    for (; i <= 5;)
+    {
+        Console.WriteLine($"C# For Loop:Iteration {i}");
+        i++;
+    }
+}
+
+
+// infinite loop
+{
+    for (int i = 1; i > 0; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+
+// nested for loop
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            Console.WriteLine(i+" "+j);
+        }
+    }
+}
+
+*/
