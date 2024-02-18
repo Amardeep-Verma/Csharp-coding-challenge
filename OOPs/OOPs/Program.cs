eec842e1d5965aa255636352687673da972552f9
Amardeep <vermaamardeep86@gmail.com>
2024-02-18 19:13:02 +0530

    OOPs

---
+++ OOPs/OOPs/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
